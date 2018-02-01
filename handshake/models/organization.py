f798d340fe6af8d8d219f8181636d6806bad3afa
Shreyas Satish <shreyas.satish@gmail.com>
2018-02-01 21:32:37 +0530

    rm permissions from org for now

--- handshake/models/organization.py
+++ handshake/models/organization.py
@@ -6,9 +6,3 @@ from flask_lastuser.sqlalchemy import ProfileBase
 
 class Organization(ProfileBase, db.Model):
     __tablename__ = 'organization'
-
-    def permissions(self, user, inherited=None):
-        perms = super(Organization, self).permissions(user, inherited)
-        if self.userid in user.organizations_owned_ids():
-            perms.add('org_admin')
-        return perms
