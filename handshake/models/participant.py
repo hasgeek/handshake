68b6b45a420c2a9a38899bcca0f77e159d5679b6
Shreyas Satish <shreyas.satish@gmail.com>
2018-02-01 21:36:38 +0530

    rm methods temporarily. to be added in the next PR

--- handshake/models/participant.py
+++ handshake/models/participant.py
@@ -47,51 +47,9 @@ class Participant(BaseMixin, db.Model):
     workspace = db.relationship(Workspace,
         backref=db.backref('participants', cascade='all, delete-orphan'))
 
-    @classmethod
-    def get(cls, current_space, current_email):
-        return cls.query.filter_by(workspace=current_space, email=current_email).one_or_none()
-
-    @classmethod
-    def upsert(cls, current_space, current_email, **fields):
-        participant = cls.get(current_space, current_email)
-        if participant:
-            participant._set_fields(fields)
-        else:
-            participant = cls(workspace=current_space, email=current_email, **fields)
-            db.session.add(participant)
-        return participant
-
-    def add_events(self, events):
-        for event in events:
-            if event not in self.events:
-                self.events.append(event)
-
-    def remove_events(self, events):
-        for event in events:
-            if event in self.events:
-                self.events.remove(event)
-
-    @classmethod
-    def checkin_list(cls, event):
-        """Return participant details along with their associated ticket types as a comma-separated string.
-        WARNING: This query uses `string_agg` and hence will only work in PostgreSQL >= 9.0
-        """
-        participant_list = db.session.query('id', 'fullname', 'email', 'company', 'twitter', 'puk', 'key', 'checked_in', 'badge_printed', 'ticket_type_titles').from_statement(text('''
-            SELECT distinct(participant.id), participant.fullname, participant.email, participant.company, participant.twitter,
-                participant.puk, participant.key, attendee.checked_in, participant.badge_printed,
-            (SELECT string_agg(title, ',') FROM sync_ticket
-                INNER JOIN ticket_type ON sync_ticket.ticket_type_id = ticket_type.id
-                WHERE sync_ticket.participant_id = participant.id) AS ticket_type_titles
-            FROM participant INNER JOIN attendee ON participant.id = attendee.participant_id
-            LEFT OUTER JOIN sync_ticket ON participant.id = sync_ticket.participant_id
-            WHERE attendee.event_id = {event_id}
-            ORDER BY participant.fullname
-        '''.format(event_id=event.id))).all()
-        return participant_list
-
 
 class KeyPair(BaseMixin, db.Model):
-    """A keypair for a participant inside a workspace."""
+    """A revocable keypair for a participant inside a workspace."""
 
     __tablename__ = 'keypair'
 
@@ -99,6 +57,5 @@ class KeyPair(BaseMixin, db.Model):
     participant = db.relationship('Participant',
         backref=db.backref('keypair', cascade='all, delete-orphan'))
     # public key
-    # FIXME: Are two keys really required?
     puk = db.Column(db.Unicode(44), nullable=False, default=make_public_key, unique=True)
     key = db.Column(db.Unicode(44), nullable=False, default=make_private_key, unique=True)
--- handshake/models/ticket_client.py
+++ handshake/models/ticket_client.py
@@ -18,33 +18,3 @@ class TicketClient(BaseScopedNameMixin, db.Model):
     workspace_id = db.Column(db.Integer, db.ForeignKey('workspace.id'), nullable=False)
     workspace = db.relationship(Workspace,
         backref=db.backref('ticket_clients', cascade='all, delete-orphan'))
-
-    def import_from_list(self, ticket_list):
-        """
-        Batch upserts the tickets and its associated ticket types and participants.
-        Cancels the tickets in cancel_list.
-        """
-
-        for ticket_dict in ticket_list:
-            ticket_type = TicketType.upsert(self.workspace, current_title=ticket_dict['ticket_type'])
-
-            participant = Participant.upsert(self.workspace, ticket_dict['email'],
-                fullname=ticket_dict['fullname'],
-                phone=ticket_dict['phone'],
-                twitter=ticket_dict['twitter'],
-                company=ticket_dict['company'],
-                job_title=ticket_dict['job_title'],
-                city=ticket_dict['city']
-            )
-
-            ticket = Ticket.get(self, ticket_dict.get('order_no'), ticket_dict.get('ticket_no'))
-            if ticket and (ticket.participant is not participant or ticket_dict.get('status') == u'cancelled'):
-                # Ensure that the participant of a transferred or cancelled ticket does not have access to
-                # this ticket's events
-                ticket.participant.remove_events(ticket_type.events)
-
-            if ticket_dict.get('status') == u'confirmed':
-                ticket = Ticket.upsert(self, ticket_dict.get('order_no'), ticket_dict.get('ticket_no'),
-                    participant=participant, ticket_type=ticket_type)
-                # Ensure that the new or updated participant has access to events
-                ticket.participant.add_events(ticket_type.events)
